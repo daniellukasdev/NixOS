5329997358c9410b3a9a2d5cd59289c4a2480b13
daniellukasdev <dtlukasart@gmail.com>
2025-10-12 11:47:38 -0400

    cleanup

--- hosts/config/nix/nixpkgs.nix
+++ hosts/config/nix/nixpkgs.nix
@@ -9,7 +9,6 @@
 
     overlays = [
       inputs.nix-vscode-extensions.overlays.default
-      # inputs.nixpkgs-wayland.overlay
       inputs.nur.overlays.default
       inputs.rust-overlay.overlays.default
     ];
--- hosts/dellxps/disk-configuration.nix
+++ hosts/dellxps/disk-configuration.nix
@@ -1,15 +1,4 @@
 {
-  # fileSystems."/" = {
-  #   device = "/dev/disk/by-label/NixOS-ROOT";
-  #   fsType = "ext4";
-  # };
-
-  # fileSystems."/boot" = {
-  #   device = "/dev/disk/by-label/NixOS-BOOT";
-  #   fsType = "vfat";
-  # };
-
-  # swapDevices = [{device = "/dev/disk/by-label/NixOS-SWAP";}];
   disko.devices = {
     disk = {
       main = {
@@ -62,48 +51,4 @@
       };
     };
   };
-  # disko.devices = {
-  #   disk = {
-  #     main = {
-  #       type = "disk";
-  #       device = "/dev/nvme0n1";
-  #       content = {
-  #         type = "gpt";
-  #         partitions = {
-  #           ESP = {
-  #             size = "1G";
-  #             type = "EF00";
-  #             content = {
-  #               type = "filesystem";
-  #               format = "vfat";
-  #               mountpoint = "/boot";
-  #               mountOptions = ["umask=0077"];
-  #             };
-  #           };
-  #           root = {
-  #             size = "100%";
-  #             content = {
-  #               type = "btrfs";
-  #               extraArgs = ["-f"];
-  #               subvolumes = {
-  #                 "/root" = {
-  #                   mountpoint = "/";
-  #                   mountOptions = ["compress=zstd" "noatime"];
-  #                 };
-  #                 "/home" = {
-  #                   mountpoint = "/home";
-  #                   mountOptions = ["compress=zstd" "noatime"];
-  #                 };
-  #                 "/nix" = {
-  #                   mountpoint = "/nix";
-  #                   mountOptions = ["compress=zstd" "noatime"];
-  #                 };
-  #               };
-  #             };
-  #           };
-  #         };
-  #       };
-  #     };
-  #   };
-  # };
 }
